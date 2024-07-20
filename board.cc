98cf7f9eecd86ff97267c08450e2975268c78d4c
Adam Troiani <actroian@uwaterloo.ca>
2024-07-20 15:44:30 -0400

    moved setup to board class

--- board.cc
+++ board.cc
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-Board::Board() : p1Turn{true} {
+Board::Board() : p1Turn{true}, custom{false} {
     // initialize empty board
-    for (int i = 0; i <= 7; ++i) {
-        vector<unique_ptr<Piece>> row(8);
-        board.push_back(std::move(row));
-    }
-    resetBoard();
+  for (int i = 0; i <= 7; ++i) {
+    vector<unique_ptr<Piece>> row(8);
+    board.push_back(std::move(row));
+  }
 }
 
+bool Board::isCustom() const { return custom; }
 void Board::resetBoard() {
     clearBoard();
     board[0][0] = make_unique<Rook>(0, 0, *this, false);
@@ -61,7 +61,7 @@ void Board::print(std::ostream& out) const {
 }
 
 void Board::placePiece(int row, int col, unique_ptr<Piece>&& piece) {
-    board[row][col] = std::move(piece);
+  board[row][col] = std::move(piece);
 }
 
 void Board::removePiece(int row, int col) {
@@ -82,4 +82,75 @@ void Board::clearBoard() {
             board[r][c] = nullptr;
         }
     }
+    custom = false;
+}
+
+void Board::setup() {
+  clearBoard();
+  
+  custom = true;
+  string cmd, arg1, arg2;
+
+  cout << "Entered setup mode." << endl;
+
+  while (true) {
+    cmd = getInput("setup command", validSetupCommands);
+    if (cmd == "+" ) {
+      arg1 = getInput("type of piece", validPieces);
+      arg2 = getInput("position on the board", boardLocations);
+
+      pair<int, int> location = posToInd[arg2];
+      
+      unique_ptr<Piece> p = nullptr;
+      if (arg1 == "p") {
+        p = make_unique<Pawn>(location.first, location.second, *this, isP1Turn());
+      }
+      if (arg1 == "k") {
+        p = make_unique<King>(location.first, location.second, *this, isP1Turn());
+      }
+      if (arg1 == "q") {
+        p = make_unique<Queen>(location.first, location.second, *this, isP1Turn());
+      }
+      if (arg1 == "b") {
+        p = make_unique<Bishop>(location.first, location.second, *this, isP1Turn());
+      }
+      if (arg1 == "r") {
+        p = make_unique<Rook>(location.first, location.second, *this, isP1Turn());
+      }
+      if (arg1 == "n") {
+        p = make_unique<Knight>(location.first, location.second, *this, isP1Turn());
+      }
+
+      if (at(location.first, location.second) == nullptr) cout << "Placed piece " << p->getSymbol() << " at " << arg2 << endl;
+      else cout << "Replaced existing piece at " << arg2 << endl;
+
+      placePiece(location.first, location.second, std::move(p));
+
+      print(cout);
+    }
+    else if (cmd == "-") {
+      arg1 = getInput("position on the board", boardLocations);
+
+      pair<int, int> location = posToInd[arg1];
+
+      if (at(location.first, location.second) != nullptr) cout << "Removed piece " << at(location.first, location.second)->getSymbol() << " at: " << arg1 << endl;
+      removePiece(location.first, location.second);
+
+      print(cout);
+    }
+    else if (cmd == "=") {
+      arg1 = getInput("colour", validColours);
+      
+      if (arg1 == validColours[0]) {
+        setP1Turn(true);
+        cout << "Next turn set to White." << endl;
+      }
+      if (arg1 == validColours[1]) {
+        setP1Turn(false);
+        cout << "Next turn set to Black." << endl;
+      }
+    }
+    else if (cmd == "done") break;
+  }
+  // TODO: implement setup mode validation
 }
