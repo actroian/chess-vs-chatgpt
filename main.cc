199aca48cd2efdc60d0b7c2b9ee4c6f47196818f
Adam Troiani <actroian@uwaterloo.ca>
2024-07-20 15:46:32 -0400

    setup mode only works when games arent in progress

--- main.cc
+++ main.cc
@@ -3,69 +3,37 @@
 #include "globals.h"
 using namespace std;
 
-const string HUMAN = "human";
-const string COMPUTER = "computer";
-const string L1 = "computer1";
-const string L2 = "computer2";
-const string L3 = "computer3";
-const string L4 = "computer4";
-
-unique_ptr<Player> createPlayer(string& input, bool isWhite) {
-  if (input == HUMAN) {
-    return make_unique<Human>(isWhite);
-  }
-  else if (input == COMPUTER) {
-    return make_unique<Computer>(isWhite);
-  }
-  //else if (input == L1) {
-  //  return make_unique<Computer>(isWhite);
-  //}
-  //else if (input == L2) {
-  //  return make_unique<Computer>(isWhite);
-  //}
-  //else if (input == L3) {
-  //  return make_unique<Computer>(isWhite);
-  //}
-  //else if (input == L4) {
-  //  return make_unique<Computer>(isWhite);
-  //}
-  else throw runtime_error("missing strings in validInputs vector");
-}
-
 int main() {
   string cmd, arg1, arg2;
-  unique_ptr<Game> game;
-  Board board;
+  Game game;
 
   while (cin >> cmd) {
     if (cmd == "game" ) {
       arg1 = getInput("player", validPlayers);
-      unique_ptr<Player> p1 = createPlayer(arg1, true);
-
       arg2 = getInput("player", validPlayers);
-      unique_ptr<Player> p2 = createPlayer(arg2, false);
-
-      game = make_unique<Game>(cout, std::move(p1), std::move(p2), board);
       
-      game->print();
+      game.beginGame(arg1, arg2);
     }
     else if (cmd == "resign") {
-      if (!game->board.isP1Turn()) {
+      if (!game.board->isP1Turn()) {
         cout << endl << "Black resigns. ";
-        game->endGame(0);
+        game.endGame(0);
       }
-      if (game->board.isP1Turn()) {
+      if (game.board->isP1Turn()) {
         cout << endl << "White resigns. ";
-        game->endGame(1);
+        game.endGame(1);
       }
-      game->reset();
+      game.reset();
     }
     else if (cmd == "move") {
       cin >> arg1 >> arg2;
 
     }
     else if (cmd == "setup") {
-      game->setup();
+      if (!game.isInGame()) {
+        game.board->setup();
+      }
+      else cout << "Cannot enter setup mode during a match." << endl;
     }
     else if (cmd == "q") break;
   }
