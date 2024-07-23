004177634c708aaf4ff4a72d167d911e5d59e907
Adam Troiani <actroian@uwaterloo.ca>
2024-07-23 18:11:46 -0400

    redesigned move class

--- move.cc
+++ move.cc
@@ -1,30 +1,15 @@
 #include "move.h"
-#include "board.h"
 using namespace std;
 
-Move::Move(pair<int, int> start, pair<int, int> end, unique_ptr<Board>& board)
-    : start(start), end(end), board(board){
-        isWhiteAttacking = board->at(start.first, start.second)->isWhitePiece();
-        isCapturingMove = board->at(end.first, end.second) == nullptr;
-      }
+Move::Move(pair<int, int> start, pair<int, int> end)
+    : start{start}, end{end}{}
 
-pair<int, int> Move::getStartPos() {
-    return make_pair(start.first, start.second);
-}
-
-pair<int, int> Move::getEndPos() {
-    return make_pair(end.first, end.second);
-}
-
-pair<pair<int, int>, pair<int, int>> Move::getMove() {
-    return make_pair(getStartPos(), getEndPos());
+Move Move::getMove() const {
+    return Move{start, end};
 }
 
 bool Move::operator==(const Move& other) const {
-    return start == other.start &&
-           end == other.end &&
-           isWhiteAttacking == other.isWhiteAttacking &&
-           isCapturingMove == other.isCapturingMove;
+    return start == other.start && end == other.end;
 }
 
 bool Move::operator!=(const Move& other) const {
