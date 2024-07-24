c8c6602f4318337fd6d9bcaba221d2d18428bf94
Jacob Zhou <zhoujaco9220@gmail.com>
2024-07-24 16:02:52 -0400

    levelone test case

--- L1.cc
+++ L1.cc
@@ -4,6 +4,6 @@ L1::L1(bool isWhite) : Player{isWhite} {}
 
 Move L1::chooseMove(unique_ptr<Board>& b) {
     vector<Move> allMoves = possibleMoves(b);
-    int randomMove = rand() % (allMoves.size()-1);
+    int randomMove = rand() % (allMoves.size() -1);
     return allMoves[randomMove];
 }
