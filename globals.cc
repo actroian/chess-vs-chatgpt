263116667a5d2cf87dc9b6b136d59cc570c09455
Adam Troiani <actroian@uwaterloo.ca>
2024-07-24 15:38:59 -0400

    updated game setup to account for new bots

--- globals.cc
+++ globals.cc
@@ -3,7 +3,7 @@
 #include "player.h"
 using namespace std;
 
-vector<const string> validPlayers = {"human", "computer"/*,"L1", "L2", "L3", "L4"*/};
+vector<const string> validPlayers = {"human", "L1", "L2", "L3"};
 vector<const string> validPieces = {"p", "b", "k", "q", "n", "r", "P", "B", "K", "Q", "N", "R"};
 vector<const string> validPromotions = {"r", "k", "b", "q"};
 vector<const string> validSetupCommands = {"+", "-", "=", "done"};
