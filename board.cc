579f2c663eccd95136e38b80268d384caa103e29
elaimizrahi <mizrahi.elai@gmail.com>
2024-07-27 18:02:08 -0400

    undo castle

--- board.cc
+++ board.cc
@@ -207,6 +207,10 @@ void Board::undo() {
   prevMoves.pop();
   placePiece(lastMove.start.first, lastMove.start.second, std::move(at(lastMove.end.first, lastMove.end.second)));
   removePiece(lastMove.end.first, lastMove.end.second);
+  if(lastMove.isCastleMove){
+    placePiece(lastMove.rookstart.first, lastMove.rookstart.second, std::move(at(lastMove.rookend.first, lastMove.rookend.second)));
+    removePiece(lastMove.rookend.first, lastMove.rookend.second);
+  }
   if(lastMove.captured_piece){
     string piece = string{lastMove.captured_piece};
     placePiece(lastMove.end.first, lastMove.end.second, createPiece(piece, lastMove.end));
--- move.cc
+++ move.cc
@@ -1,8 +1,8 @@
 #include "move.h"
 using namespace std;
 
-Move::Move(pair<int, int> start, pair<int, int> end, char captured_piece)
-    : start{start}, end{end}, captured_piece{captured_piece} {}
+Move::Move(pair<int, int> start, pair<int, int> end, char captured_piece, pair<int, int> rookstart, pair<int, int> rookend, bool isCastleMove)
+    : start{start}, end{end}, captured_piece{captured_piece}, rookstart{rookstart}, rookend{rookend}, isCastleMove{isCastleMove} {}
 
 bool Move::operator==(const Move& other) const {
     return start == other.start && end == other.end;
