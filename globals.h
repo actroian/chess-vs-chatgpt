3947993a8cbed994872524c28feda9c36e4c42aa
Adam Troiani <actroian@uwaterloo.ca>
2024-07-28 11:45:57 -0400

    added instructions to main command interface

--- globals.h
+++ globals.h
@@ -15,6 +15,7 @@ extern std::map<const std::pair<int,int>, std::string> indToPos;
 extern std::vector<const std::string> validPlayers;
 extern std::vector<const std::string> validSetupCommands;
 extern std::vector<const std::string> validPromotions;
+extern std::vector<const std::string> validMainCommands;
 
 void initializePosToInd();
 std::string getInput(const std::string&, std::vector<const std::string>&);
