e758367b454c6163ec159a67ce66d7864a6ee6e3
elaimizrahi <mizrahi.elai@gmail.com>
2024-07-29 08:48:59 -0400

    formatting & const errors on linux env

--- L3.cc
+++ L3.cc
@@ -11,7 +11,7 @@ unique_ptr<Move> L3::chooseMove(unique_ptr<Board>& b, Player* p2) {
         randomMove = rand() % avoid.size();
         return std::move(avoid[randomMove]);
     }
-    L2::chooseMove(b, p2);
+    return L2::chooseMove(b, p2);
     
 }
 
@@ -20,9 +20,6 @@ vector<unique_ptr<Move>> L3::avoidCaptureMoves(unique_ptr<Board>& b, Player* p2)
     vector<unique_ptr<Move>> allMoves = possibleMoves(b, p2);
     vector<unique_ptr<Move>> avoid;
 
-    for(const auto& move : allMoves) {
-        // check if current square is being attacked by opponent pieces?
-    }
 
     return avoid;
 }
